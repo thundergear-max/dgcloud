53a1478ebc7d504e56f77fd16dc8bdeee88638a6
thundergear-max <52620272+thundergear-max@users.noreply.github.com>
2021-08-06 12:50:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ function ImageGrid({ setSelectedImg }) {
                 />
               </a>
             </motion.div>
-<a href={urls} target='_blank' download="MyExampleDoc">
+<a href={urls}  download>
             <button
               style={{
                 backgroundColor: "blue",
